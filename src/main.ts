02d54f13ad8ab51c840862302efab0dcf8973c5e
Iran Batista <iran@iranbatista.dev>
2026-05-04 22:04:20 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
